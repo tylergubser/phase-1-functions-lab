76c56d50606f5eb196a96486d0ce201b2cd09546
tylergubser <tylergubser2200@gmail.com>
2022-02-01 17:11:30 -0600

    commit tree b212beee1d7bad80b1209fe75c4bc3b9ca58a1c0

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks(someValue) {  
+   
+    return Math.abs(someValue - 42);
+
+}
+
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264;
+
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start < destination) {
+    
+        return (destination - start) * 264;
+    }
+    else {
+        return (start - destination) * 264;
+    }
+}
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) < 400) {
+        return 0;
+    }else if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) < 2000)
+    return (distanceTravelledInFeet(start, destination) - 400) * .02;
+    else if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) < 2500)
+    return 25;
+    else (distanceTravelledInFeet(start, destination) > 2500)
+    return 'cannot travel that far';
+}
+
